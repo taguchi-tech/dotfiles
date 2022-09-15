8defb35494408621a459cae33646a9943d5b0197
taguchi <taguchi.tech@gmail.com>
2022-09-15 11:56:25 +0900

    backup original dotfiles if exists

--- .bash_profile
+++ .bash_profile
@@ -8,7 +8,8 @@ fi
 # User specific environment and startup programs
 
 # PATH=/usr/local/sbin:/usr/local/bin:$PATH
-LANG=ja_JP.UTF-8
+#LANG=ja_JP.UTF-8
+LANG=en_US.UTF-8
 
 export PATH LANG
 unset USERNAME
--- install.sh
+++ install.sh
@@ -2,12 +2,32 @@
 
 set -eu
 
-link_to_dotfiles() {
-    for file in .bash_profile .bashrc .hg .tmux.conf .vim .vimrc .zprofile .zshrc
-    do
-        #[ ! -e $file ] && ln -s dotfiles/$file .
-        [ ! -e $HOME/$file ] && ln -s $HOME/dotfiles/$file $HOME/$file
-    done
+backup() {
+  target=$1
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.orig"
+      echo "-----> Moved your old $target config file to $target.orig"
+    fi
+  fi
 }
 
-link_to_dotfiles
+symlink() {
+  file=$1
+  link=$2
+  if [ ! -e "$link" ]; then
+    echo "-----> Symlinking your new $link"
+    ln -s $file $link
+  fi
+}
+
+cd $HOME
+
+for file in .bash_profile .bashrc .hgrc .tmux.conf .vim .vimrc .zprofile .zshrc; do
+    target="${HOME}/${file}"
+    if [ ! -d "$target" ]; then
+        backup $target
+        symlink ${HOME}/dotfiles/${file} $target
+    fi
+done
+
